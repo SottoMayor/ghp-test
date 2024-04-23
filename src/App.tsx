b55f5fbddce0d1014e5b5b77f0bac7b10314b478
David Sotto Mayor <ddsmmf@gmail.com>
2024-04-23 13:36:31 -0400

    b

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, Route, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import { Route, createRoutesFromElements, RouterProvider, createHashRouter } from 'react-router-dom';
 
 import Home from './Pages/Home';
 import Home00WeBSVmenu from './Pages/Home00WeBSVmenu';
@@ -36,7 +36,7 @@ function App() {
     </Route>
   )
 
-  const router = createBrowserRouter(routesDef, { basename: '/ghp-test' }); 
+  const router = createHashRouter(routesDef, { basename: '/ghp-test' }); 
 
   return (
 
